b87766ee2be01a1f6b195d9db92eb4520bc732eb
Aditya Chaurasia <t.aditya.chaurasia@e-arc.com>
2025-10-03 11:07:58 +0530

    Initial Commit

---
+++ SwiftUIAssignment/SwiftUIAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAssignmentApp.swift
+//  SwiftUIAssignment
+//
+//  Created by Aditya Chaurasia on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
